62ee163c45bb4d0d311acc94eac6c9c1ae4705d0
Benjamin Sproule <benjamin@benjaminsproule.com>
2021-07-09 23:58:15 +0100

    Add f1-micro compute instance

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "google" {
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
