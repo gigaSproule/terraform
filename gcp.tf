748407083640dc238b025c683ba76ba63b6ad34d
Benjamin Sproule <benjamin@benjaminsproule.com>
2021-08-01 22:45:59 +0100

    Set allow stopping for update

--- gcp.tf
+++ gcp.tf
@@ -5,8 +5,9 @@ provider "google" {
 }
 
 resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "e2-micro"
+  name                      = "terraform-instance"
+  machine_type              = "e2-micro"
+  allow_stopping_for_update = true
 
   boot_disk {
     initialize_params {
